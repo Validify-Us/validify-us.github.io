47abb179eb9b7a43e81ee619f70504192872313b
Shaked Lanciano <shakedlanciano@gmail.com>
2023-05-23 15:00:02 +0300

    nav is responsive and done

--- src/lib/components/Nav.svelte
+++ src/lib/components/Nav.svelte
@@ -11,9 +11,31 @@
             navToggle = false
         }
     }
+
+    import navlogo from '$lib/assets/navicon.png'
+    import closelogo from '$lib/assets/close-logo.png'
 </script>
 
+
 <div>
-<button class=" bg-slate-500 px-3 py-2" on:click={toggleNavbar}>togglecheck</button>
-<p class="{!navToggle ? "hidden" : "block"}">im showing if true</p>
+    <div class="flex items-center p-8 justify-between">
+        <h1 class=" text-3xl font-bold text-[#5f00e3]">Validify.</h1>
+        <ul class="hidden md:flex gap-4">
+            <a class="hover:text-[#5f00e3] duration-300 ease-in-out capitalize hover:font-[600]" href="/about">about us</a>
+            <a class="hover:text-[#5f00e3] duration-300 ease-in-out capitalize hover:font-[600]" href="/contact">contact us</a>
+            <a class="hover:text-[#5f00e3] duration-300 ease-in-out capitalize hover:font-[600]" href="/team"> our team</a>
+        </ul>
+        <img class=" {navToggle ? "hidden" : "block"} md:hidden w-[4rem] hover:cursor-pointer" on:click={toggleNavbar} src={navlogo} alt="nav icon">
+        <img class="{!navToggle ? "hidden" : "block"} md:hidden w-[3rem] hover:cursor-pointer" on:click={toggleNavbar} src={closelogo} alt="nav icon">
+
+    </div>
+    <div class="{!navToggle ? "hidden" : "flex"} bg-[#350a70] p-8 w-[100%] h-max justify-center items-center gap-4 md:hidden shadow-lg">
+        <ul class="flex flex-col justify-center items-center text-center text-white">
+            <a class=" hover:text-[#5f00e3] duration-300 ease-in-out capitalize hover:font-[600]" href="/about">about us</a>
+            <a class="hover:text-[#5f00e3] duration-300 ease-in-out capitalize hover:font-[600]" href="/contact">contact us</a>
+            <a class="hover:text-[#5f00e3] duration-300 ease-in-out capitalize hover:font-[600]" href="/team"> our team</a>
+        </ul>
+    </div>
+    
+
 </div>
