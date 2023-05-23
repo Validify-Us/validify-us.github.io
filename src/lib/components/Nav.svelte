bafc1de6869ff3072e79cb20ee4ec4a96bcf9f8f
Shaked Lanciano <shakedlanciano@gmail.com>
2023-05-23 12:16:22 +0300

    testing nav toggle

---
+++ src/lib/components/Nav.svelte
@@ -0,0 +1,19 @@
+<script>
+    let navToggle = false
+    function toggleNavbar()
+    {
+        if (navToggle === false)
+        {
+            navToggle = true
+        }
+        else
+        {
+            navToggle = false
+        }
+    }
+</script>
+
+<div>
+<button class=" bg-slate-500 px-3 py-2" on:click={toggleNavbar}>togglecheck</button>
+<p class="{!navToggle ? "hidden" : "block"}">im showing if true</p>
+</div>
