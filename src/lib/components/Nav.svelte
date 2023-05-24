c6237150145239bb8a39a8e83de2c7028bcf0247
Izzy <121457100+IzzyFisher@users.noreply.github.com>
2023-05-24 00:18:27 -0500

    Update Nav.svelte
    removed "about us" link

--- src/lib/components/Nav.svelte
+++ src/lib/components/Nav.svelte
@@ -22,7 +22,6 @@
         <a href="/" class=" text-3xl font-bold text-[#5f00e3]">Validify.</a>
         <ul class="hidden md:flex gap-4">
             <a class="hover:text-[#5f00e3] duration-300 ease-in-out capitalize hover:font-[600]" href="/">home</a>
-            <a class="hover:text-[#5f00e3] duration-300 ease-in-out capitalize hover:font-[600]" href="/about">about us</a>
             <a class="hover:text-[#5f00e3] duration-300 ease-in-out capitalize hover:font-[600]" href="#">contact us</a>
             <a class="hover:text-[#5f00e3] duration-300 ease-in-out capitalize hover:font-[600]" href="/team"> our team</a>
         </ul>
@@ -33,7 +32,6 @@
     <div class="{!navToggle ? "hidden" : "flex"} bg-[#350a70] p-8 w-[100%] h-max justify-center items-center gap-4 md:hidden shadow-lg">
         <ul class="flex flex-col justify-center items-center text-center text-white">
             <a class=" hover:text-[#5f00e3] duration-300 ease-in-out capitalize hover:font-[600]" href="/">home</a>
-            <a class=" hover:text-[#5f00e3] duration-300 ease-in-out capitalize hover:font-[600]" href="/about">about us</a>
             <a class="hover:text-[#5f00e3] duration-300 ease-in-out capitalize hover:font-[600]" href="#">contact us</a>
             <a class="hover:text-[#5f00e3] duration-300 ease-in-out capitalize hover:font-[600]" href="/team"> our team</a>
         </ul>
