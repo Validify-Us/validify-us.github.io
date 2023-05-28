1344d340a554820e9d79f70d3a1e1ab76c3d14dd
Shaked Lanciano <88950077+MrsHorrid@users.noreply.github.com>
2023-05-28 13:59:49 +0300

    implemented proper seo now

--- src/lib/components/Seo.svelte
+++ src/lib/components/Seo.svelte
@@ -1,12 +1,25 @@
 
 <svelte:head>
-    <meta property="og:title" content="Validify" />
-    <meta property="og:url" content="validify-us.com" />
-    <meta property="og:image" content="https://imgur.com/a/jOS1qFI" />
-    <meta name="description" content="Validify a company dedicated to blockchain security , AI and devops optimizations.">
-    <meta name="keywords" content="blockchain, ai , devops , technology , tech , high tech">
-    <meta name="twitter:title" content="Validify">
-    <meta name="twitter:description" content="Validify a company dedicated to blockchain security, AI and devops optimizations.">
-    <meta name="twitter:image" content="https://imgur.com/a/jOS1qFI">
+    <!-- OPEN GRAPH META DATA -->
+    <meta property="og:url" content="https://validify-us.com">
+    <meta property="og:type" content="website">
+    <meta property="og:site_name" content="Validify">
+    <meta property="og:title" content="Validify">
+    <meta property="og:description" content="Validify is a company dedicated to blockchain AI and optimization">
+    <meta property="og:image" content="https://i.ibb.co/XFJjWnV/Frame-9.png">
+
+    <!-- TWITTER META DATA -->
     <meta name="twitter:card" content="summary_large_image">
+    <meta property="twitter:domain" content="https://validify-us.com">
+    <meta property="twitter:url" content="https://validify-us.com">
+    <meta name="twitter:title" content="Validify">
+    <meta name="twitter:description" content="Validify is a company dedicated to blockchain AI and optimization">
+    <meta name="twitter:image" content="https://i.ibb.co/XFJjWnV/Frame-9.png">
+    <meta data-rh="true" name="twitter:image:alt" content="validify preview">
+    <meta data-rh="true" name="twitter:image:width" content="1280">
+    <meta data-rh="true" name="twitter:image:height" content="720">
+    <meta name="twitter:site" content="@Validify_us">
+    <meta name="twitter:creator" content="@Validify_us">
+    <meta name="twitter:creator:id" content="Validify_us">
+
 </svelte:head>
